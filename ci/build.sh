e6e4ec11f198e8ffe86dd8ddd0e808d1b815fb50
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2020-04-21 12:57:11 +0100

    build matrix test

--- ci/build.sh
+++ ci/build.sh
@@ -2,21 +2,10 @@
 
 set -ex
 
-COMPILER="$1"; shift
-RELEASE="$1"; shift
-ABI="$1"; shift
-BLOSC="$1"; shift
-SIMD="$1"; shift
-CMAKE_EXTRA="$@"
-
 mkdir build
 cd build
 cmake \
-    -DCMAKE_CXX_COMPILER=${COMPILER} \
-    -DCMAKE_BUILD_TYPE=${RELEASE} \
-    -DOPENVDB_ABI_VERSION_NUMBER=${ABI} \
     -DOPENVDB_USE_DEPRECATED_ABI=ON \
-    -DUSE_BLOSC=${BLOSC} \
     -DOPENVDB_BUILD_PYTHON_MODULE=ON \
     -DOPENVDB_BUILD_UNITTESTS=ON \
     -DOPENVDB_BUILD_BINARIES=ON \
@@ -24,7 +13,6 @@ cmake \
     -DOPENVDB_BUILD_VDB_LOD=ON \
     -DOPENVDB_BUILD_VDB_RENDER=ON \
     -DOPENVDB_BUILD_VDB_VIEW=ON \
-    -DOPENVDB_SIMD=${SIMD} \
-    ${CMAKE_EXTRA} \
+    $@ \
     ..
 make -j2
