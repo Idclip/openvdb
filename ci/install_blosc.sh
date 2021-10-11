5c5ae8a285b5d3919d39eea925962afd8ae5457d
Nick Avramoussis <4256455+Idclip@users.noreply.github.com>
2021-10-12 00:11:35 +0100

    blosc tests

--- ci/install_blosc.sh
+++ ci/install_blosc.sh
@@ -13,6 +13,11 @@ fi
 
 mkdir build
 cd build
-cmake ../.
+
+# On MacOS there's a bug between blosc 1.14-1.20 where unistd isn't included
+# in zlib-1.2.8/gzlib.c. Provide -DPREFER_EXTERNAL_ZLIB to use the installed
+# version of zlib.
+# https://github.com/Blosc/python-blosc/issues/229
+cmake -DPREFER_EXTERNAL_ZLIB=ON -DBUILD_STATIC=OFF -DBUILD_TESTS=OFF -DBUILD_FUZZERS=OFF -DBUILD_BENCHMARKS=OFF ../.
 make -j4
 make install
